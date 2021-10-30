186ed8382fbfd2fe596d85537e21bf66dfec9189
Kasper Notebomer <k.a.notebomer@st.hanze.nl>
2021-10-30 21:27:57 +0200

    Change CSVFile to FileHandler due to renaming of the class, remove imports

--- src/main/java/nl/bioinf/knotebomer/actaeon/ActaeonLauncher.java
+++ src/main/java/nl/bioinf/knotebomer/actaeon/ActaeonLauncher.java
@@ -1,15 +1,12 @@
 package nl.bioinf.knotebomer.actaeon;
 
 import weka.core.Instances;
-import weka.core.converters.CSVLoader;
-
-import java.io.File;
 
 public class ActaeonLauncher {
     public static void main(String[] args) {
         // TODO
         try{
-        CsvFile file = new CsvFile();
+        FileHandler file = new FileHandler();
         Instances instances = file.readCSVToInstances("data/clean_data.csv");
         // System.out.println(instances);
 
