dbc70b777b9cf4a9442ccefa32a55591ab3427ab
Kasper Notebomer <k.a.notebomer@st.hanze.nl>
2021-10-30 23:30:42 +0200

    Change FileHandler to CSVHandler due to name change

--- src/main/java/nl/bioinf/knotebomer/actaeon/ActaeonLauncher.java
+++ src/main/java/nl/bioinf/knotebomer/actaeon/ActaeonLauncher.java
@@ -6,8 +6,8 @@ public class ActaeonLauncher {
     public static void main(String[] args) {
         // TODO
         try{
-        FileHandler file = new FileHandler();
-        Instances instances = file.readCSVToInstances("data/clean_data.csv");
+        CSVHandler file = new CSVHandler();
+        Instances instances = file.readFile("data/clean_data.csv");
         // System.out.println(instances);
 
         Classifier classifier = new Classifier();
