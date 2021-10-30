f7879dc4d0e81ee43982f75ce2bab101caa8cec7
Kasper Notebomer <k.a.notebomer@st.hanze.nl>
2021-10-30 20:54:39 +0200

    Add some test code to main

--- src/main/java/nl/bioinf/knotebomer/actaeon/ActaeonLauncher.java
+++ src/main/java/nl/bioinf/knotebomer/actaeon/ActaeonLauncher.java
@@ -1,10 +1,25 @@
 package nl.bioinf.knotebomer.actaeon;
 
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+
 import java.io.File;
 
 public class ActaeonLauncher {
     public static void main(String[] args) {
         // TODO
+        try{
+        CsvFile file = new CsvFile();
+        Instances instances = file.readCSVToInstances("data/clean_data.csv");
+        // System.out.println(instances);
+
+        Classifier classifier = new Classifier();
+        Instances labeled = classifier.classifyInstances(instances);
+        // System.out.println(labeled);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     void start(OptionProvider optionProvider) {
