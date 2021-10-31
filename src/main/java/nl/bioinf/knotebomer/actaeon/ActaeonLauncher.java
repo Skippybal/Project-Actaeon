4b940981acce6eeec4c0bc94ba1e72a67e15fc29
Kasper Notebomer <k.a.notebomer@st.hanze.nl>
2021-10-31 15:29:21 +0100

    Fix commandline input for predictions

--- src/main/java/nl/bioinf/knotebomer/actaeon/ActaeonLauncher.java
+++ src/main/java/nl/bioinf/knotebomer/actaeon/ActaeonLauncher.java
@@ -29,9 +29,11 @@ public class ActaeonLauncher {
 
     void start(OptionProvider optionProvider) {
         try {
+            Instances instances;
             String inputFile = optionProvider.getFilePath();
             Boolean printToCommandLine = optionProvider.getPrintToCMD();
             FileHandler<String, String> inputHandler;
+            //System.out.println(optionProvider.getCharacteristics());
             if (inputFile != null) {
                 //String fileType = FilenameUtils.getExtension(inputFile);
                 String fileType = optionProvider.getInputFileExtention();
@@ -43,26 +45,33 @@ public class ActaeonLauncher {
                 }
 
                 //assert inputHandler != null;
-                Instances instances = inputHandler.readFile(inputFile);
-
-                Classifier classifier = new Classifier();
-                Instances labeled = classifier.classifyInstances(instances);
-                if (printToCommandLine) {
+                System.out.println(inputFile);
+                instances = inputHandler.readFile(inputFile);
+            } else if (optionProvider.getCharacteristics() != null){
                     CMDHandler cmdHandler = new CMDHandler();
-                    cmdHandler.writeFile(labeled, "");
-                }
+                    instances = cmdHandler.readFile(optionProvider.getCharacteristics());
+            } else {
+                throw new IllegalArgumentException("No valid arguments found");
+            }
 
-                FileHandler<String, String> outputHandler;
-                String outFile = optionProvider.getFilePath();
-                if (outFile != null){
-                    String outFileType = optionProvider.getOutputFileExtention();
-                    if (outFileType.equals("csv")) {
-                        outputHandler = new CSVHandler();
-                    } else {
-                        outputHandler = new ArffHandler();
-                    }
-                    outputHandler.writeFile(labeled, optionProvider.getOutputFile());
+            Classifier classifier = new Classifier();
+            Instances labeled = classifier.classifyInstances(instances);
+            if (printToCommandLine) {
+                CMDHandler cmdHandler = new CMDHandler();
+                cmdHandler.writeFile(labeled, "");
+            }
+
+            FileHandler<String, String> outputHandler;
+            String outFile = optionProvider.getOutputFile();
+            if (outFile != null){
+                String outFileType = optionProvider.getOutputFileExtention();
+                if (outFileType.equals("csv")) {
+                    outputHandler = new CSVHandler();
+                } else {
+                    outputHandler = new ArffHandler();
                 }
+                System.out.println(outFile);
+                outputHandler.writeFile(labeled, optionProvider.getOutputFile());
             }
         } catch (Exception e){
             e.printStackTrace();
