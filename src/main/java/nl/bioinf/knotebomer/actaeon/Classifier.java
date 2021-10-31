53fa37888836f870550375c16df3676b3befac11
Kasper Notebomer <k.a.notebomer@st.hanze.nl>
2021-10-31 19:17:14 +0100

    Divide classifyInstances method
    The necessary datasets(Instances objects) are now created in a separate function and added as class variables

--- src/main/java/nl/bioinf/knotebomer/actaeon/Classifier.java
+++ src/main/java/nl/bioinf/knotebomer/actaeon/Classifier.java
@@ -1,26 +1,29 @@
 package nl.bioinf.knotebomer.actaeon;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 
 import weka.classifiers.trees.LMT;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Attribute;
-import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.SerializationHelper;
-import weka.core.converters.ArffLoader;
 
 public class Classifier {
     private RandomForest sterolPresentModel;
     private RandomForest tailClassifierModel;
     private LMT sterolConcentrationModel;
+    private Instances scaledInstances;
+    private Instances scaledInstancesLMT;
+    private Instances labeledInstances;
 
     public Classifier() {
         loadModel();
     }
 
+    /**
+     * Load all the models for classification
+     */
     private void loadModel(){
         try{
 
@@ -37,13 +40,50 @@ public class Classifier {
         }
     }
 
+    /**
+     * Classify all the instances in the given dataset
+     * @param instances (a dataset)
+     * @return LabeledInstances
+     * @throws Exception
+     */
     public Instances classifyInstances(Instances instances) throws Exception {
 
+        makeDataSet(instances);
+
+        for (int i = 0; i < labeledInstances.numInstances(); i++) {
+            labeledInstances.setClassIndex(labeledInstances.numAttributes() - 3);
+            scaledInstances.setClassIndex(scaledInstances.numAttributes() - 3);
+            double labelSterolPresent = sterolPresentModel.classifyInstance(scaledInstances.instance(i));
+            //String clsLabelSterolPresent = labeled.classAttribute().value((int)labelSterolPresent);
+            labeledInstances.instance(i).setClassValue(labelSterolPresent);
+
+            labeledInstances.setClassIndex(labeledInstances.numAttributes() - 2);
+            scaledInstances.setClassIndex(scaledInstances.numAttributes() - 2);
+            double labelTails = tailClassifierModel.classifyInstance(scaledInstances.instance(i));
+            //String clsLabelTails = labeled.classAttribute().value((int)labelTails);
+            labeledInstances.instance(i).setClassValue(labelTails);
+
+            labeledInstances.setClassIndex(labeledInstances.numAttributes() - 1);
+            double labelSterolConcentration = sterolConcentrationModel.classifyInstance(scaledInstancesLMT.instance(i));
+            //String clsLabelSterolConcentration = instances.classAttribute().value((int)labelSterolConcentration);
+            labeledInstances.instance(i).setClassValue(labelSterolConcentration);
+        }
+        System.out.println(labeledInstances);
+        return labeledInstances;
+    }
+
+    /**
+     * Creates the necessary datasets for classification
+     * @param instances (a dataset)
+     * @throws Exception
+     */
+    private void makeDataSet(Instances instances) throws Exception {
+
+        // Create the needed attributes
         ArrayList<String> sterolPresentValues = new ArrayList<>(2);
         sterolPresentValues.add("yes");
         sterolPresentValues.add("no");
         Attribute sterolPresent = new Attribute("sterol.present", sterolPresentValues);
-
 
         ArrayList<String> tailValues = new ArrayList<>(5);
         tailValues.add("DI");
@@ -60,42 +100,30 @@ public class Classifier {
         sterolConcentrationValues.add("30");
         Attribute sterolConcentration = new Attribute("sterol.conc", sterolConcentrationValues);
 
+        //Standardize the instances
         MinMax minMax = new MinMax();
         Instances scaledData = minMax.minMaxer(instances);
 
+        //Build a special dataset for the LMT tree classification
         Instances scaledDataLMT = new Instances(scaledData);
         scaledDataLMT.insertAttributeAt(sterolConcentration, scaledDataLMT.numAttributes());
         scaledDataLMT.setClassIndex(scaledDataLMT.numAttributes() - 1);
 
+        //Add attributes
         scaledData.insertAttributeAt(sterolPresent, scaledData.numAttributes());
         scaledData.insertAttributeAt(tails, scaledData.numAttributes());
         scaledData.insertAttributeAt(sterolConcentration, scaledData.numAttributes());
 
+        //Create dataset where all of the labels can be inserted
         Instances labeled = new Instances(instances);
 
         labeled.insertAttributeAt(sterolPresent, labeled.numAttributes());
         labeled.insertAttributeAt(tails, labeled.numAttributes());
         labeled.insertAttributeAt(sterolConcentration, labeled.numAttributes());
 
-        for (int i = 0; i < labeled.numInstances(); i++) {
-            labeled.setClassIndex(labeled.numAttributes() - 3);
-            scaledData.setClassIndex(scaledData.numAttributes() - 3);
-            double labelSterolPresent = sterolPresentModel.classifyInstance(scaledData.instance(i));
-            //String clsLabelSterolPresent = labeled.classAttribute().value((int)labelSterolPresent);
-            labeled.instance(i).setClassValue(labelSterolPresent);
-
-            labeled.setClassIndex(labeled.numAttributes() - 2);
-            scaledData.setClassIndex(scaledData.numAttributes() - 2);
-            double labelTails = tailClassifierModel.classifyInstance(scaledData.instance(i));
-            //String clsLabelTails = labeled.classAttribute().value((int)labelTails);
-            labeled.instance(i).setClassValue(labelTails);
-
-            labeled.setClassIndex(labeled.numAttributes() - 1);
-            double labelSterolConcentration = sterolConcentrationModel.classifyInstance(scaledDataLMT.instance(i));
-            //String clsLabelSterolConcentration = instances.classAttribute().value((int)labelSterolConcentration);
-            labeled.instance(i).setClassValue(labelSterolConcentration);
-        }
-        System.out.println(labeled);
-        return labeled;
+        //Set data as class variables
+        this.scaledInstances = scaledData;
+        this.scaledInstancesLMT = scaledDataLMT;
+        this.labeledInstances = labeled;
     }
 }
