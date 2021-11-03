67ac8e326ad008b1cc2a5fc9fb892b2b3a631949
Kasper Notebomer <k.a.notebomer@st.hanze.nl>
2021-11-03 16:55:13 +0100

    Remove redundant classes

--- src/main/java/nl/bioinf/knotebomer/actaeon/WekaClassifierModel.java
+++
@@ -1,9 +0,0 @@
-package nl.bioinf.knotebomer.actaeon;
-
-import weka.core.Instances;
-
-import java.util.ArrayList;
-
-public interface WekaClassifierModel {
-    public double classifyInstances(Instances instances) throws Exception;
-}
