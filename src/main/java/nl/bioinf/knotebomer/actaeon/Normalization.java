8472509f45fa8aa3391db9ed3e2c1d163ffe3abd
Kasper Notebomer <k.a.notebomer@st.hanze.nl>
2021-11-04 14:23:45 +0100

    Get training instances from resources in the Normalization class

--- src/main/java/nl/bioinf/knotebomer/actaeon/Normalization.java
+++ src/main/java/nl/bioinf/knotebomer/actaeon/Normalization.java
@@ -1,7 +1,11 @@
 package nl.bioinf.knotebomer.actaeon;
 
 import weka.core.Instances;
+import weka.core.converters.CSVLoader;
 import weka.filters.unsupervised.attribute.Normalize;
+
+import java.io.File;
+import java.util.Objects;
 
 
 public class Normalization {
@@ -13,17 +17,25 @@ public class Normalization {
      * @throws Exception
      */
     public Instances normalize(Instances instances) throws Exception {
-        CSVHandler loader = new CSVHandler();
-        Instances train = loader.readFile("data/dataFrame_all_sims.csv");
+
+        // Load training instances from the resources
+        ClassLoader classLoader = getClass().getClassLoader();
+        File trainingDataFile = new File(Objects.requireNonNull(classLoader.getResource("data/dataFrame_all_sims.csv")).getFile());
+
+        CSVLoader loader = new CSVLoader();
+        loader.setSource(trainingDataFile);
+        Instances trainingInstances = loader.getDataSet();
 
         Normalize filter = new Normalize();
-        filter.setInputFormat(train);
+        filter.setInputFormat(trainingInstances);
 
-        for (int i = 0; i < train.numInstances(); i++){
-            filter.input(train.instance(i));
+        // Create the filter
+        for (int i = 0; i < trainingInstances.numInstances(); i++){
+            filter.input(trainingInstances.instance(i));
         }
         filter.batchFinished();
 
+        // Apply the filter to the given instances
         Instances normalizedInstances = filter.getOutputFormat();
         for (int i = 0; i < instances.numInstances(); i++){
             filter.input(instances.instance(i));
